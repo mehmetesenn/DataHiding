711757001823d996da4c79ea6e2f9722919d3a98
mehmetesenn <mehmetesen7183@gmail.com>
2022-04-28 14:05:21 +0300

    final

--- app/src/main/java/com/mehmetesen/encryption/MainActivity.java
+++ app/src/main/java/com/mehmetesen/encryption/MainActivity.java
@@ -3,10 +3,13 @@ package com.mehmetesen.encryption;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +24,39 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this,Encode.class);
         startActivity(intent);
         finish();
+
     }
 
     public void Decode(View view) {
         Intent intent = new Intent(MainActivity.this,Decode.class);
         startActivity(intent);
+        finish();
+    }
+
+    public void Restart(View view) {
+        Intent intent = new  Intent(MainActivity.this,HomeActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void Settings(View view) {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        intent.setData(uri);
+        startActivity(intent);
+        finish();
+    }
+
+    public void Developer(View view) {
+        //url https://tr.linkedin.com/in/mehmet-esenn
+        Intent intent = new Intent(Intent.ACTION_VIEW,Uri.parse("https://tr.linkedin.com/in/mehmet-esenn"));
+        startActivity(intent);
+        finish();
+
+    }
+
+
+    public void Exit(View view) {
         finish();
     }
 }
